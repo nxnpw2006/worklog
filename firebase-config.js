a888ec1b09b78086200db2483f7ca49e8606b28c
nxnpw2006 <phaktheema80@gmail.com>
2026-04-24 22:33:08 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAEXdJVD5ZlWhQW3b9w2j7TcdF4hih555E",
+  authDomain: "worklog-app-48d04.firebaseapp.com",
+  projectId: "worklog-app-48d04",
+  storageBucket: "worklog-app-48d04.firebasestorage.app",
+  messagingSenderId: "1000954828407",
+  appId: "1:1000954828407:web:05920537b8fa938718d872",
+  measurementId: "G-JPY32WCRQY"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
