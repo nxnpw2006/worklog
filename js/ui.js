510d2eca851059f131db66e9f7e0403d843bd504
nxnpw2006 <phaktheema80@gmail.com>
2026-05-05 14:09:43 +0700

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,15 +1,8 @@
-function renderTable(data) {
-  const el = document.getElementById("table");
-
-  el.innerHTML = data.map(d => `
-    <div>
-      ${d.date} | ${d.total} บาท
-      <button onclick="del('${d.id}')">ลบ</button>
-    </div>
-  `).join("");
-}
-
-async function del(id) {
-  await deleteEntry(id);
-  loadData();
+function render(data){
+ let html='<table><tr><th>Date</th><th>Hours</th><th>Pay</th></tr>';
+ data.forEach(d=>{
+  html+=`<tr><td>${d.date}</td><td>${d.hours}</td><td>${d.pay}</td></tr>`;
+ });
+ html+='</table>';
+ document.getElementById('table').innerHTML=html;
 }
