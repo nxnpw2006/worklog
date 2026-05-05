c7fc566ab084dedac1693ff9a8249da0aaedc328
nxnpw2006 <phaktheema80@gmail.com>
2026-05-05 14:16:19 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -10,3 +10,18 @@ function loginGoogle(){
         alert(error.message);
     });
 }
+function logout(){
+    firebase.auth().signOut()
+    .then(()=> window.location = "login.html");
+}
+firebase.auth().onAuthStateChanged(user => {
+    if(user){
+        if(location.pathname.includes("login.html")){
+            window.location = "index.html";
+        }
+    }else{
+        if(location.pathname.includes("index.html")){
+            window.location = "login.html";
+        }
+    }
+});
